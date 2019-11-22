532edb8f6881c80116df1aa46ac2d9f620867575
Foram <foram1209@gmail.com>
2019-11-22 13:09:03 +0530

    [IMP]gmt_machine:View enhanced

--- gmt_machine/models/machine.py
+++ gmt_machine/models/machine.py
@@ -9,17 +9,6 @@ from odoo import api,fields,models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-#    @api.model
-#    def _setup_fields(self):
-#        
-#        cls = type(self)
-#        type_selection = cls._fields['type'].selection
-#        if PRODUCT_TYPE not in type_selection:
-#            tmp = list(type_selection)
-#            tmp.append(PRODUCT_TYPE)
-#            cls._fields['type'].selection = tuple(set(tmp))
-#        super()._setup_fields()
-    
     type = fields.Selection(selection_add=[('machine', 'Machine')])
     program_name = fields.Char('Program name')
     mode = fields.Char("Mode")
@@ -29,7 +18,4 @@ class ProductTemplate(models.Model):
     feed_override = fields.Char("Feed Override")
     rpm_override = fields.Char("RPM Override")
     machine_image_ids = fields.One2many('machine.image','machine_id','Machines')
-#    activity_log_ids = fields.One2many('activity.log','machine_id','Activity Logs')
-#    oee_ids = fields.One2many('overall.equipment.effectiveness','machine_id','OEE')
-#    qc_ids = fields.One2many('quality.control','machine_id','QC')
-    
+
